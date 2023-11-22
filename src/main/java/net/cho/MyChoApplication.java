a0605c9ab377205eeb43e811ab9d459468d3dfee
fobu2009 <newsun46@naver.com>
2023-11-22 14:44:02 +0900

    commit

---
+++ src/main/java/net/cho/MyChoApplication.java
@@ -0,0 +1,13 @@
+package net.cho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChoApplication.class, args);
+	}
+
+}
